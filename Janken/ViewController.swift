c6a5b35e2a8d63b6a4637c96588f40a8b87c7c71
ucmuller <ucmuller@gmail.com>
2019-01-25 10:49:33 +0900

    Initial Commit

---
+++ Janken/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Janken
+//
+//  Created by Yu Shimura on 2019/01/25.
+//  Copyright © 2019 Yu Shimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
